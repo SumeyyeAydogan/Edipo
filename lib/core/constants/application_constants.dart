c3aaad3382ee44e0774349fb1e8ebbe23b5c5552
enesonmez <sonmezens@hotmail.com>
2022-11-27 17:53:56 +0300

    bottom-navigation-bar-olusturuldu

--- lib/core/constants/application_constants.dart
+++ lib/core/constants/application_constants.dart
@@ -1,4 +1,4 @@
 class ApplicationConstants {
-  static const String APP_NAME = "Carry Vibe";
-  static const String VALIDATE_FORM_ERROR = "Bu alan zorunludur.";
+  final String appName = "Edypo";
+  final String validateFormError = "Bu alan zorunludur.";
 }
